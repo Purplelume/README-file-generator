800c06c866dddfa41a14c782e1f0cfef0c5dcf25
Your Name <you@example.com>
2023-04-16 20:38:28 +1000

    Implemented code without licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,118 @@
-// TODO: Include packages needed for this application
+// Install packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: "What is this project title?",
+        name: "Project title"
+    },
+    {
+        type: 'input',
+        message: "What is the project description?",
+        name: "Description"
+    },
+    {
+        type: 'input',
+        message: "What are the installation instructions?",
+        name: "Installation"
+    },
+    {
+        type: 'input',
+        message: "What are the usage instructions?",
+        name: "Usage"
+    },
+    {
+        type: 'checkbox',
+        message: "What is the license for this project?",
+        choices: ["None", "MIT", "Mozzilla", "Eclipse"],
+        name: "License"
+    },
+    {
+        type: 'input',
+        message: "What are the instructions for contributing?",
+        name: "Contributions"
+    },
+    {
+        type: 'input',
+        message: "What are the commands to test the application?",
+        name: "Test"
+    },
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: "Username"
+    },
+    {
+        type: 'input',
+        message: "What is your email address?",
+        name: "Email"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+const writeToFile = data => {
+    
+    // Gets the project title and stores it into a variable
+    let projectTitle = data["Project title"];
+    
+    // Creates the README file with the answers of the user
+const writeText = 
+`# ${projectTitle} 
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${data["Description"]}
+
+### Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation  
+${data["Installation"]}
+
+## Usage
+${data["Usage"]}
+
+## License
+
+## Contributions
+${data["Contributions"]}
+
+## Tests
+Do as shown here to test the application:
+${data["Test"]}
+
+## Questions?
+Come and take a look at my GitHub (${data["Username"]}) or reach out to me: Email me at ${data["Email"]}`;
+
+    // Assign to the README the project title
+    let fileName = `Test-README.md`;
+
+    if (projectTitle) {
+        // Formatting the text in a good manner
+        projectTitle = projectTitle.trim().split(" ").join("-");
+        fileName = `${projectTitle}-README.md`;
+    }
+
+    // Writes file using fs
+    fs.writeFile(fileName, writeText, (error) => {
+        error ? console.error(error) : console.log("README generated");
+    })
+}
+
+// Function to initialize the app
+function init() {
+
+    inquirer.prompt(questions)
+    .then (answers => writeToFile(answers))
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,66 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// example url https://shields.io/badge/license-MIT-green
 
-// TODO: Create a function that returns the license link
+function renderLicenseBadge(license) {
+  let licenseBadge = "";
+
+  if(license != "None") {
+    licenseBadge = "![License Badge](https://shields.io/badge/license-" + license + "-blue)";
+  }
+
+  return licenseBadge;
+}
+
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
 
-// TODO: Create a function that returns the license section of README
+  // select correct license link for the selected license
+  switch(license) {
+    case "MIT":
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "Mozzilla":
+      licenseLink = "https://opensource.org/licenses/MPL-2.0";
+      break;
+    case "Eclipse":
+      licenseLink = "https://opensource.org/licenses/EPL-1.0";
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  
+  return licenseLink;
+}
+
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
 
-// TODO: Create a function to generate markdown for README
+  // if a license has been selected, create License section
+  // with link to license information
+  if (license != "None") {
+    licenseSection += "## License\n"
+    licenseSection += "Please see " + renderLicenseLink(license) + " to get detailed information for this license\n";
+  }
+
+  return licenseSection;
+}
+
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  // License badge added
+  let markdown = "# " + renderLicenseBadge(data.license) + "\n";
+
+  // License added
+  markdown += renderLicenseSection(data.license) + "\n";
+
+  return markdown;
 }
 
 module.exports = generateMarkdown;
